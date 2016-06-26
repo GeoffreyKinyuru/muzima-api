b61e28ceda8da013e82b441746fec006493b7ca7
ProgrammerJeff <ProgrammerJeff@PROGRAMMERS-PC>
2016-06-26 09:58:45 +0300

    MUZIMA-409 Create field to save Obs edited status

--- src/main/java/com/muzima/api/model/Observation.java
+++ src/main/java/com/muzima/api/model/Observation.java
@@ -147,7 +147,7 @@ public class Observation extends OpenmrsSearchable {
         this.voided = voided;
     }
     /**
-    * The following is an additional code
+    * THE following is an additional code
     * The code enhances the user interations with the Obs
     * It allows users to understand the current state of data in the respective obs form cells
     * Its not yet complete
